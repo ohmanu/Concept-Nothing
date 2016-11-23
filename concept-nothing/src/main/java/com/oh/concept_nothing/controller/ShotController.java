de57e12df9e027e6e08ece4aeebcb38600e35ff5
ohmanu <casastoscano@gmail.com>
2016-11-23 20:33:04 +0100

    Fixes & new dependencies.

--- concept-nothing/src/main/java/com/oh/concept_nothing/controller/ShotController.java
+++ concept-nothing/src/main/java/com/oh/concept_nothing/controller/ShotController.java
@@ -1,11 +1,14 @@
 package com.oh.concept_nothing.controller;
 
+import java.util.Collections;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.oh.concept_nothing.domain.Shot;
 import com.oh.concept_nothing.service.ShotService;
 import com.oh.concept_nothing.service.UserService;
 
@@ -18,8 +21,12 @@ public class ShotController {
 
 	@GetMapping("/shots")
 	public String listAll(HttpServletRequest request) {
-		//request.setAttribute("shots", shotservice.findAll());
-		request.setAttribute("shots", userservice.findById(0).getShots());
+		Iterable<Shot> shots = shotservice.findAll();
+		
+		if(shots != null)
+			request.setAttribute("shots", shotservice.findAll());
+		else
+			request.setAttribute("shots", Collections.emptyList());
 		
 		return "gallery/shots";
 	}
